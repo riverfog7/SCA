a8059c08449125ef3df8b7ef7f62f6620464959f
riverfog7 <cjw9770@gmail.com>
2025-11-29 23:50:53 +0900

    Alternative prepare_model_for_kbit_training func for qwen3 omni

---
+++ src/sca_train/utils.py
@@ -0,0 +1,17 @@
+def prepare_model_for_kbit_training(model, use_gradient_checkpointing: bool = True, gradient_checkpointing_kwargs = None):
+    if use_gradient_checkpointing:
+        model.gradient_checkpointing_enable(gradient_checkpointing_kwargs)
+
+    # Automatic fallback for other models
+    if hasattr(model, "thinker"):
+        # Qwen3 Omni will use this
+        embed_tokens = model.thinker.model.embed_tokens
+    elif hasattr(model, "model") and hasattr(model.model, "embed_tokens"):
+        embed_tokens = model.model.embed_tokens
+    else:
+        raise AttributeError("Could not locate embed_tokens in model structure.")
+
+    def make_inputs_require_grad(module, input, output):
+        output.requires_grad_(True)
+
+    embed_tokens.register_forward_hook(make_inputs_require_grad)
