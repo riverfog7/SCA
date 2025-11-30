590407c1672ace6c885244c92b531de8c4a7a5dd
riverfog7 <cjw9770@gmail.com>
2025-11-30 15:46:47 +0900

    Disable infiniband

--- scripts/train.sh
+++ scripts/train.sh
@@ -4,6 +4,7 @@ SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 ACCELERATE_CONFIG="${SCRIPT_DIR}/../configs/accelerate/memory_efficient_2gpus.yaml"
 SCA_CONFIG="${SCRIPT_DIR}/../configs/sca/default.yaml"
 export NCCL_P2P_DISABLE=1
+export NCCL_IB_DISABLE=1
 
 uv run accelerate launch \
   --config_file "${ACCELERATE_CONFIG}" \
