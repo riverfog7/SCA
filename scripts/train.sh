03b193a87705a2abdd9b92a3e3e92626088f9a10
riverfog7 <cjw9770@gmail.com>
2025-12-04 15:39:11 +0000

    Working configs

--- scripts/train.sh
+++ scripts/train.sh
@@ -3,11 +3,12 @@
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 ACCELERATE_CONFIG="${SCRIPT_DIR}/../configs/accelerate/memory_efficient_2gpus.yaml"
 SCA_CONFIG="${SCRIPT_DIR}/../configs/sca/default.yaml"
-export NCCL_P2P_DISABLE=1
 export NCCL_IB_DISABLE=1
 
 export OMP_NUM_THREADS=1
 export MKL_NUM_THREADS=1
+
+export TOKENIZERS_PARALLELISM=false
 
 cd "${SCRIPT_DIR}/.." || exit 1
 uv run accelerate launch \
