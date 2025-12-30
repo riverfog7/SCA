a6d5307970576b0d6d7ec871cff98a6d58f8dba2
riverfog7 <cjw9770@gmail.com>
2025-12-30 18:10:34 +0900

    feat: Support for audio outputs in data collator

--- src/sca_train/data_collator.py
+++ src/sca_train/data_collator.py
@@ -3,10 +3,11 @@ from transformers import Qwen3OmniMoeProcessor
 
 
 class Qwen3OmniCollator:
-    def __init__(self, processor: Qwen3OmniMoeProcessor, mask_instruction: bool = True, max_length: int = 32768):
+    def __init__(self, processor: Qwen3OmniMoeProcessor, mask_instruction: bool = True, max_length: int = 32768, train_talker: bool = False):
         self.processor = processor
         self.mask_instruction = mask_instruction
         self.max_length = max_length
+        self.train_talker = train_talker
 
         assert hasattr(processor, "tokenizer"), "Processor must have a tokenizer attribute"
         self.pad_token_id = processor.tokenizer.pad_token_id
@@ -24,22 +25,52 @@ class Qwen3OmniCollator:
 
         texts = []
         audios = []
+        assistant_audios = []
 
         # 2. Extract Data
         for feature in features:
             messages = feature["messages"]
             feature_audio = None
+            assistant_audio = None
+            user_found = False
+            assistant_found = False
 
             for msg in messages:
                 if msg["role"] == "user":
+                    if user_found:
+                        raise ValueError("Multiple user messages found in a single input.")
+                    user_found = True
                     for content in msg["content"]:
                         if content["type"] == "audio":
                             if "audio_waveform" in content:
+                                if feature_audio is not None:
+                                    raise ValueError("Multiple user audio contents found in a single input.")
                                 feature_audio = content.pop("audio_waveform")
                                 content["audio_url"] = "place_holder"
 
+                if msg["role"] == "assistant":
+                    if assistant_found:
+                        raise ValueError("Multiple assistant messages found in a single input.")
+                    if not user_found:
+                        raise ValueError("Assistant message found before user message.")
+                    assistant_found = True
+
+                    remove_idx = []
+                    for i, content in enumerate(msg["content"]):
+                        if content["type"] == "audio":
+                            if "audio_waveform" in content:
+                                if assistant_audio is not None:
+                                    raise ValueError("Multiple assistant audio contents found in a single input.")
+                                assistant_audio = content.pop("audio_waveform")
+                                assistant_audios.append(assistant_audio)
+                                remove_idx.append(i)
+                    for idx in reversed(remove_idx):
+                        del msg["content"][idx]
+
             if feature_audio is None:
                 feature_audio = np.zeros(16000)
+            if self.train_talker ^ (assistant_audio is not None):
+                raise ValueError("Mismatch in talker training mode and presence of assistant audio.")
 
             audios.append(feature_audio)
 
@@ -67,12 +98,20 @@ class Qwen3OmniCollator:
             labels[labels == self.pad_token_id] = -100
 
         # Apply Instruction Masking
+        # Data always ends with assistant response and is single-turn
         if self.mask_instruction and self.im_start_id is not None:
             for i in range(len(batch["input_ids"])):
                 starts = (batch["input_ids"][i] == self.im_start_id).nonzero(as_tuple=True)[0]
                 if len(starts) > 0:
                     last_start = starts[-1]
-                    labels[i, :last_start] = -100
+                    header_offset = 3
+                    labels[i, :last_start + header_offset] = -100
+
+                # system, user, assistant -> 3 starts max
+                if len(starts) > 3:
+                    raise ValueError("Multiple <|im_start|> tokens found in a single-turn input.")
 
         batch["labels"] = labels
+        if len(assistant_audios) > 0:
+            batch["assistant_audios"] = assistant_audios
         return batch
